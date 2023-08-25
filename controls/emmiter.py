56c1a620630e017b8a408853a3daa5aedc5ccc12
Raghav Bhargava <rbhar017@uottawa.ca>
2023-08-25 15:40:20 -0400

    configure socket

---
+++ controls/emmiter.py
@@ -0,0 +1,24 @@
+import socket            
+ 
+s = socket.socket()        
+print ("Socket successfully created")
+
+port = 12345               
+ 
+s.bind(('100.84.40.97', port))        
+print ("socket binded to %s" %(port))
+ 
+while True:
+ 
+# Establish connection with client.
+  c, addr = s.accept()    
+  print ('Got connection from', addr )
+ 
+  # send a thank you message to the client. encoding to send byte type.
+  c.send('Thank you for connecting'.encode())
+ 
+  # Close the connection with the client
+  c.close()
+   
+  # Breaking once connection closed
+  break
