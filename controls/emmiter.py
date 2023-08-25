ec13e5758336a4ed8ddbffc4b56da2cf90b0a0e1
Raghav Bhargava <rbhar017@uottawa.ca>
2023-08-25 15:45:56 -0400

    ,

--- controls/emmiter.py
+++ controls/emmiter.py
@@ -2,12 +2,18 @@ import socket
  
 s = socket.socket()        
 print ("Socket successfully created")
-
+ 
 port = 12345               
  
 s.bind(('100.84.40.97', port))        
 print ("socket binded to %s" %(port))
  
+# put the socket into listening mode
+s.listen(5)    
+print ("socket is listening")           
+ 
+# a forever loop until we interrupt it or
+# an error occurs
 while True:
  
 # Establish connection with client.
