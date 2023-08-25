4f34075d697fd976aa583f8900723cbc9fdd1e11
Raghav Bhargava <rbhar017@uottawa.ca>
2023-08-25 15:56:59 -0400

    .

--- raspi/piSocket.py
+++ raspi/piSocket.py
@@ -10,7 +10,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen()
     conn, addr = s.accept()
     with conn:
-        print(f'Connected by {addr}')
+        print('Connected by ' + addr)
         while True:
             data = conn.recv(1024)
             if not data:
